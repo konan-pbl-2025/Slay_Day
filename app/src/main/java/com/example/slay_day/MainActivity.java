a749037e64918e086fb617eaed1353a55c148227
mnt1231 <s2471060@s.konan-u.ac.jp>
2025-10-31 17:35:01 +0900

    ホームが画面からゲーム画面に映るプログラムを作りました。

--- app/src/main/java/com/example/slay_day/MainActivity.java
+++ app/src/main/java/com/example/slay_day/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.slay_day;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button playButton = (Button) findViewById(R.id.button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
