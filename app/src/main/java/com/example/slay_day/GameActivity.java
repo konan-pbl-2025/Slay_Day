f31cf62f3c781f52f9fe4c57bc9938846cdf54f7
greeh <tatuya239@icloud.com>
2025-11-07 13:50:16 +0900

    てすと

--- app/src/main/java/com/example/slay_day/GameActivity.java
+++ app/src/main/java/com/example/slay_day/GameActivity.java
@@ -9,8 +9,32 @@ public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+    }
+
+
+
+
+
+
+
+
+    public void cardRandom(){
+        Random rand = new Random();
+        ArrayList <Integer> cardColler = new ArrayList<>();
+        ArrayList <Integer> cardNum = new ArrayList<>();
+        ArrayList <Integer> cardType = new ArrayList<>();
+        for(int i=0;i<5;i++){
+            cardType.add(1);
+        }
+        for(int i=0;i<5;i++){
+            cardColler.add(rand.nextInt(3));
+        }
+        for(int i=0;i<5;i++){
+            cardNum.add(rand.nextInt(5)+1);
+        }
     }
 }
